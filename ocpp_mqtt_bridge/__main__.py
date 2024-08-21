b0851a49e54696f0cb93b76628ef9199b3b91d5d
Michael Allan <michael@intrinseca.me.uk>
2024-08-21 20:53:54 +0100

    Tidy up logging config

--- ocpp_mqtt_bridge/__main__.py
+++ ocpp_mqtt_bridge/__main__.py
@@ -1,12 +1,19 @@
 import asyncio
-import logging
+import logging.config
 import os
 import sys
+
+import tomllib
 
 from .cs import main
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    os.makedirs("logs", exist_ok=True)
+
+    with open("logging_config.toml", "rb") as logging_config_file:
+        logging_config = tomllib.load(logging_config_file)
+
+    logging.config.dictConfig(logging_config)
 
     # Change to the "Selector" event loop if platform is Windows
     if sys.platform.lower() == "win32" or os.name.lower() == "nt":
